fd1eb366ee233fb3dad71341d99cd9c12c96e988
revido <revidoberliano@gmail.com>
2022-12-13 23:40:36 +0700

    stories

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 import './App.css';
-import styled from 'styled-components'
-import Header from './components/Header'
-import Sidebar from './components/Sidebar'
+
 function App() {
   return (
-    <AppWrapper>
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-      </div>
-    </AppWrapper >
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-const AppWrapper = styled.div`
-  background-color: #f1f2f5;
-  .app__body {
-    display: flex;
-  }
-`
+
 export default App;
